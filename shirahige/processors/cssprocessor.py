121d85e79b1ec01c656070eb65b7d92c2f291113
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2014-06-02 07:35:58 +0900

    CSS parsing implemented

--- shirahige/processors/cssprocessor.py
+++ shirahige/processors/cssprocessor.py
@@ -1,6 +1,7 @@
 # -*- encoding:utf-8 -*-
 
 from shirahige.processors.baseprocessor import BaseProcessor
+from shirahige.utilities import Url
 import cssutils
 
 
@@ -8,4 +9,16 @@ class CSSProcessor(BaseProcessor):
 
     def process(self):
         BaseProcessor.process(self)
-        self.result = self.response.text.encode('utf-8')
+        css = cssutils.parseString(self.response.text.encode('utf-8'))
+        self._replace_url(css)
+        self.result = css.cssText
+
+    def _replace_url(self, css):
+        for rule in css:
+            if rule.type == rule.STYLE_RULE:
+                for p in rule.style:
+                    if isinstance(p.value,cssutils.css.URIValue):
+                        url = Url(p.value.uri, self.url.absolute())
+                        print "css inside:" + url.absolute()
+                        p.value.uri = url.hash_filename()
+                        self.app.queue.add(url)
